64987c1e2bfbbeba1da29c9da64c619a705556b0
pdomi06 <pdomi06@gmail.com>
2022-11-04 12:19:57 +0100

    Searrch bar létre hozása

--- index.js
+++ index.js
@@ -29,3 +29,18 @@ function changeBg() {
 
  window.addEventListener('scroll', changeBg)
 
+ function search() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+
