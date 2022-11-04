360e938f2ad216cf83746e514b7db21843ac51bc
pdomi06 <pdomi06@gmail.com>
2022-11-04 12:33:31 +0100

    Navbar search gomb működik

--- index.js
+++ index.js
@@ -29,17 +29,19 @@ function changeBg() {
 
  window.addEventListener('scroll', changeBg)
 
- function search() {
+ // JavaScript code
+function search_animal() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('animals');
+    let x = document.getElementsByClassName('item');
+    let list = document.getElementById('card-group')
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
         }
         else {
-            x[i].style.display="list-item";                 
+            x[i].style.display=list;                 
         }
     }
 }
